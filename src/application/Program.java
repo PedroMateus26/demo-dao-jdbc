199c995c4a7aeb925d5703637272c56ec15da63e
PedroMateus26 <pedromateusneto@gmail.com>
2020-08-26 09:25:25 -0300

    DaoFactory e SellerDao criadas

--- src/application/Program.java
+++ src/application/Program.java
@@ -9,6 +9,8 @@ import com.mysql.jdbc.Statement;
 
 import db.DB;
 import db.DbException;
+import model.dao.DaoFactory;
+import model.dao.SellerDao;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -19,6 +21,8 @@ public class Program {
 		Department department = new Department(1, "Books");
 		Seller seller =  new Seller(21,"Bob","bob@gmail.com",new Date(), 3000.0,department);
 		System.out.println(seller);
+		
+		SellerDao sellerDao = DaoFactory.createSellerDao();
 		
 		
 		/*Connection conn = null;
