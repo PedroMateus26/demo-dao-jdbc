c0d86208bf90ae843b74f1ae7f230b037c0c8090
PedroMateus26 <pedromateusneto@gmail.com>
2020-08-26 10:20:21 -0300

    FindById Implementado

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,9 +1,12 @@
 package model.dao;
 
+import com.mysql.jdbc.Connection;
+
+import db.DB;
 import model.dao.impl.SellerDaoJDBC;
 
 public class DaoFactory {
 	public static SellerDao createSellerDao() {
-		return new SellerDaoJDBC();
+		return new SellerDaoJDBC((Connection) DB.getConnection());
 	}
 }
