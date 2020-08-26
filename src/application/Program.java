d4c212f7a5911635475a8902b6e9c24078dd9ed7
PedroMateus26 <pedromateusneto@gmail.com>
2020-08-26 09:05:54 -0300

    Criando classe seller do pacote model.entities

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,20 +1,24 @@
 package application;
 
 import java.sql.Connection;
+
 import java.sql.SQLException;
+import java.util.Date;
 
 import com.mysql.jdbc.Statement;
 
 import db.DB;
 import db.DbException;
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 
 		Department department = new Department(1, "Books");
-		System.out.println(department);
+		Seller seller =  new Seller(21,"Bob","bob@gmail.com",new Date(), 3000.0,department);
+		System.out.println(seller);
 		
 		
 		/*Connection conn = null;
